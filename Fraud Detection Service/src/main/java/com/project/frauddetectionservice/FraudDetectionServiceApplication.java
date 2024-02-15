363faa6917fe0b0f8ce86410a85c8c9363215b1a
Akshay Ghavale <akshay.ghavale@s2022.tu-chemnitz.de>
2024-02-15 18:24:44 +0100

    Creation of microservices with first initial commit

---
+++ Fraud Detection Service/src/main/java/com/project/frauddetectionservice/FraudDetectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.frauddetectionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudDetectionServiceApplication.class, args);
+	}
+
+}
---
+++ Fraud Detection Service/src/test/java/com/project/frauddetectionservice/FraudDetectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.frauddetectionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
